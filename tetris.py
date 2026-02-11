4f6c747faf621e2f9f9097b92bad145842fe04ed
Marthym <marthym.gaming@gmail.com>
2026-02-11 22:30:01 +0100

    Fond d'écran animé car pourquoi pas

--- tetris.py
+++ tetris.py
@@ -2,7 +2,12 @@ import pygame
 import os
 import sys
 
-
+#COULEURS
+blueBG = 80
+maxk = 6
+blueBGdir = 1 # pour le retour inverse du bleu vers sa valeur initiale
+colorText = (255,255,255)
+colorGrid = (20,20,30)
 
 WIDTH = 500
 LENGTH = 500
@@ -14,6 +19,7 @@ class Tetris:
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, LENGTH))
+game = Tetris()
 pygame.display.set_caption("Tetris")
 
 pygame.mixer.music.load(MUSIC)
@@ -21,13 +27,35 @@ pygame.mixer.music.play(loops=-1)
 
 clock = pygame.time.Clock()
 
+def gameUpdate(screen, game):
+    """
+    Mise à jour en temps réel du jeu, fonction principale.
+    """
+
+def bgChange(k):
+    if k < maxk:
+        return
+    global blueBG, blueBGdir
+    blueBG += blueBGdir
+    if blueBG == 100:
+        blueBGdir = -1
+    if blueBG == 80:
+        blueBGdir = 1
+
 running = True
+k = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    k += 1
+    bgChange(k)
+    if k == maxk:
+        k = 0
+    screen.fill((45,45,blueBG))
 
-    screen.fill((20, 20, 20))
+    gameUpdate(screen, game)
+
     pygame.display.flip()
     clock.tick(60)
 
